9fa690eb4cb47ae3bc2081978e50f8aa68000e8d
Zac Cohen <companyhen@gmail.com>
2013-07-24 23:33:40 -0400

    Updated profile page and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -17,4 +17,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	assert_response :success
   end
 
+  test "that a profile page works" do
+    get '/zaccohen'
+    assert_response :success
+  end
 end
